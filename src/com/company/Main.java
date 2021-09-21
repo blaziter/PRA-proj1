d48f4c9f0a3926cc2b186065f8fbd808544e39ab
Petr Tran <petrblaztran@gmail.com>
2021-09-21 18:59:57 +0200

    Initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,20 +1,52 @@
 package com.company;
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
         switch (args[0]) {
-            case "h":
+            case "help", "h", "-h", "--h":
                 System.out.println("Write items for shopping list in arguments");
+                System.out.println("Available items: broccoli, shrimp, apple, Till");
                 break;
+
             default:
-                int i = 0;
+                int price = 0;
+                String[] items = {"broccoli", "shrimp", "apple", "Till"};
+                int[] itemsCount = new int[4];
+
                 for (String arg : args) {
-                    i++;
-                    System.out.println(i + ". item: " + arg);
+                        switch (Integer.parseInt(arg)) {
+                            case 1:
+                                price += 50;
+                                itemsCount[0]++;
+                                break;
+
+                            case 2:
+                                price += 100;
+                                itemsCount[1]++;
+                                break;
+
+                            case 3:
+                                price += 15;
+                                itemsCount[2]++;
+                                break;
+
+                            case 4:
+                                price += 500;
+                                itemsCount[3]++;
+                                break;
+
+                            default:
+                                System.out.println("Item " + arg + " does not exist.");
+
+                        }
                 }
+
+                for (int i = 0; i < items.length; i++) {
+                    System.out.println(itemsCount[i] + "x" + " " + items[i]);
+                }
+
+                System.out.println("Total: " + price);
                 break;
         }
 
